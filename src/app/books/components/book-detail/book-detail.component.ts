a80794b9db45b3078c23270a7065a3c30eb9d629
ibjelicic <ibjelicic@gmail.com>
2021-01-04 13:44:22 +0100

    fix: change validator to support update scenario

--- src/app/books/components/book-detail/book-detail.component.ts
+++ src/app/books/components/book-detail/book-detail.component.ts
@@ -1,8 +1,15 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  AsyncValidatorFn,
+} from "@angular/forms";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { BookModel } from "src/app/shared/models";
 import { BooksService } from "src/app/shared/services";
-import { referenceValidator } from "src/app/shared/validators/referenceValidator";
 
 @Component({
   selector: "app-book-detail",
@@ -20,11 +27,7 @@ export class BookDetailComponent {
     name: new FormControl(""),
     earnings: new FormControl(0),
     description: new FormControl(""),
-    ref: new FormControl(
-      "",
-      [Validators.required],
-      referenceValidator(this.bookService)
-    ),
+    ref: new FormControl("", [Validators.required], this.referenceValidator()),
   });
 
   @Input() set book(book: BookModel) {
@@ -51,5 +54,19 @@ export class BookDetailComponent {
   onSubmit(book: BookModel) {
     this.save.emit({ ...this.originalBook, ...book });
     this.bookForm.reset();
+  }
+
+  private referenceValidator(): AsyncValidatorFn {
+    return (
+      control: AbstractControl
+    ): Observable<{ [key: string]: any } | null> => {
+      return this.bookService
+        .checkReference(control.value)
+        .pipe(
+          map((res) =>
+            res.length > 0 && !this.originalBook ? { refExist: true } : null
+          )
+        );
+    };
   }
 }
